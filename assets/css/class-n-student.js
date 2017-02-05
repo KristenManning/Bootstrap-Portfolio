640cd08b946df66b6d2e5d00b7a611136bc64995
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2017-02-05 02:31:49 -0500

    trying out a new chalkboard theme

---
+++ assets/css/class-n-student.js
@@ -0,0 +1,26 @@
+function Class(){
+    this.student = student;
+    this.teacher = teacher;
+    this.location = location;
+    this.subect = subject;
+    this.tas = tas;
+    this.startTimeOfTheClass = startTimeOfTheClass;
+    this.endTimeOfTheClass = endTimeOfTheClass;
+    this. classClown = classClown;
+
+}
+
+function Student(){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.grade = grade;
+    this.awake = awake;
+    this.gender = gender;
+    this.hometown = hometown;
+    this.favoritePizzaTopping = favoritePizzaTopping;
+}
+
+var kristen = new Student("kristen", "manning", "22", "17", true, "female", "Glenmont", "olives")
+
+console.log(kristen)
---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$( "#a" ).click(function() {
+	  window.location = "index.html";
+	});
+	$( "#b" ).click(function() {
+	  window.location = "portfolio.html";
+	});
+	$( "#c" ).click(function() {
+	  window.location = "interests.html";
+	});
+	$( "#d" ).click(function() {
+	  window.location = "contact.html";
+	});
+});
